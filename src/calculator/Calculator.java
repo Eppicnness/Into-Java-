c9e947483dfd4206d49523b1dbed40cb4d4928f1
GMan Playz <gavinshiznit@gmail.com>
2022-11-30 10:48:33 -0600

    First commit

---
+++ src/calculator/Calculator.java
@@ -0,0 +1,15 @@
+
+
+package calculator;
+
+
+public class Calculator {
+
+    
+    public static void main(String[] args) {
+        CalcJ g = new CalcJ();
+        g.setSize(500,525);
+        g.setVisible(true);
+    }
+    
+}
